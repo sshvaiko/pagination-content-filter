f82a63f4e16435e58b1883f6d246e06cc3f75949
Stan Shvaika <stan@shvaika.com>
2017-10-02 22:51:24 +0300

    Minor fixes

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # pagination-content-filter
-
-
-
-1 1 10
-2 11 20
-3 21 30
-4 31 40
-5 41 50
-6 51 60
+Demo: https://sshvaiko.github.io/pagination-content-filter/
--- js/app.js
+++ js/app.js
@@ -2,14 +2,57 @@
 
 const postsPerPage = 10;
 const contentList = document.querySelector('.student-list');
+const notFound = document.createElement('p');
+
+// create not found message
+notFound.innerHTML = 'Sorry, nothing found.';
+notFound.style.display = 'none';
+contentList.parentNode.appendChild(notFound);
 
 const getItemsList = () => {
   return contentList.querySelectorAll('li:not(.disabled)');
 }
-let itemsList = contentList.querySelectorAll('li:not(.disabled)');
+
+const appendPageLinks = itemsList => {
+  let count = Math.ceil(itemsList.length / postsPerPage);
+  if(itemsList.length > postsPerPage) {
+    // generate and add html pagination code
+    let pagination = document.createElement('div');
+    pagination.className = 'pagination';
+
+    let pageLinksHtml = '<ul>';
+    for (let i = 1; i < count + 1; i++) {
+        pageLinksHtml += '<li><a href="#page-' + i + '">' + i + '</a></li>';
+    }
+    pageLinksHtml += '</ul>';
+    pagination.innerHTML = pageLinksHtml;
+    pagination.querySelector('a').className = 'active';
+    contentList.parentNode.appendChild(pagination);
+
+    // add events
+    const pageLinks = pagination.querySelectorAll('a');
+    for (let i = 0; i < pageLinks.length; i++) {
+      pageLinks[i].addEventListener('click', event => {
+        // remove active class from the preview active page and add to a new
+        let currentPage = pagination.querySelector('.active');
+        currentPage.classList.remove('active');
+        event.target.className = 'active';
+        showPage(event.target);
+      });
+    }
+  }
+}
+
+const updatePagination = () => {
+  // if we have already pagination we should update it
+  let oldPagination = document.querySelector('.pagination');
+  if(oldPagination) {
+    oldPagination.parentNode.removeChild(oldPagination);
+  }
+}
 
 const showPage = targetPage => {
-  targetPage.className = 'active';
+  let itemsList = getItemsList();
 
   let targetPageNumber = parseInt( (targetPage.text) );
   let firstElement = (targetPageNumber - 1) * postsPerPage;
@@ -25,29 +68,7 @@ const showPage = targetPage => {
 
 }
 
-const appendPageLinks = itemsList => {
-  let count = Math.ceil(itemsList.length / postsPerPage);
-  if(itemsList.length > postsPerPage) {
-    let pageLinks = document.createElement('div');
-    pageLinks.className = 'pagination';
-
-    let pageLinksHtml = '<ul>';
-    for (let i = 1; i < count + 1; i++) {
-        pageLinksHtml += '<li><a href="#page-' + i + '">' + i + '</a></li>';
-    }
-    pageLinksHtml += '</ul>';
-
-    pageLinks.innerHTML = pageLinksHtml;
-    pageLinks.querySelector('a').className = 'active';
-    contentList.parentNode.appendChild(pageLinks);
-
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const searchList = () => {
+const searchList = (itemsList) => {
   let searchForm = document.createElement('div');
   searchForm.className = 'student-search';
   searchForm.innerHTML = '<input placeholder="Search for students...">';
@@ -60,16 +81,17 @@ const searchList = () => {
   searchButton.addEventListener('click', event => {
     let searchQuery = searchInput.value.toLowerCase();
     let isFound = false;
-    console.log('search');
-    const resultList = [];
 
+    // if item contains query string, it adds it in array
+    const resultList = [];
     for (let i = 0; i < itemsList.length; i++) {
       let itemContent = itemsList[i].querySelector('.student-details').innerText.toLowerCase();
 
       if(itemContent.indexOf(searchQuery) === -1) {
-        itemsList[i].classList.add('disabled');
+        itemsList[i].classList.add('hidden');
+        itemsList[i].style.display = 'none';
       } else {
-        itemsList[i].classList.remove('disabled');
+        itemsList[i].classList.remove('hidden');
         isFound = true;
         resultList.push(itemsList[i]);
       }
@@ -77,43 +99,30 @@ const searchList = () => {
     };
 
     if( !isFound ) {
-      alert('Nothing found!')
+      notFound.style.display = 'block';
+      updatePagination();
     } else {
-      console.log(resultList);
-      appendPageLinks(resultList);
+      notFound.style.display = 'none';
+      // initialization with found items
+      init(resultList);
     }
 
   });
 
 }
 
-const initialization = (itemsList) => {
+const init = itemsList => {
   // show items only for the first page
   for (let i = 0; i < itemsList.length; i++) {
     if(i > postsPerPage - 1) {
       itemsList[i].style.display = 'none';
+    } else {
+      itemsList[i].removeAttribute('style');
     }
   }
-
-  // add page links with event handlers if needed pagination
-  if( appendPageLinks(itemsList) ) {
-    const pageBlock = document.querySelector('.pagination');
-    const pageLinks = pageBlock.querySelectorAll('a');
-
-    for (let i = 0; i < pageLinks.length; i++) {
-      pageLinks[i].addEventListener('click', event => {
-        // remove active class from the current page
-        let currentPage = pageBlock.querySelector('.active');
-        currentPage.classList.remove('active');
-        showPage(event.target);
-      });
-    }
-
-  }
+  updatePagination();
+  appendPageLinks(itemsList);
 }
 
-// initialization
-initialization( getItemsList() );
-
-// add search form
-searchList();
+init( getItemsList() );
+searchList( getItemsList() );
